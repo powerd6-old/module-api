536a5a4e78b9267ea04867b95fac1d6f626b95c7
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2022-06-12 23:09:26 +0200

    Create cli tool (#2)
    * fix readme text
    
    * add gitignore for nodejs projects
    
    * add cli sourcecode
    
    * add workflow to publish on release

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const log = require("loglevel");
+
+const configuration = require("./steps/configuration");
+const { loadModule } = require("./steps/content");
+const { saveModule } = require("./steps/filehandler");
+
+function buildModule() {
+  log.setDefaultLevel("INFO");
+
+  const { source, destination } = configuration.loadConfiguration();
+  const module = loadModule(source);
+  log.debug(module);
+  log.info("Loading complete.");
+
+  saveModule(module, destination);
+
+  log.info("Completed!");
+}
+
+module.exports = {
+  buildModule,
+};
